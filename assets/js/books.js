c0e69bea8cf3e675f647760ab84a933efed6b1f2
jmkuhn11 <joshuakuhn2022@u.northwestern.edu>
2023-02-09 02:01:35 -0600

    Displaying results on page

--- assets/js/books.js
+++ assets/js/books.js
@@ -1,29 +1,39 @@
 var form = document.getElementById("search-form");
 var bookPage = document.getElementById('bookPage');
+var next = document.getElementById('next');
+var previous = document.getElementById('previous');
+var pageStartIndex = 0;
+
+next.addEventListener('click', nextPage);
+previous.addEventListener('click', previousPage);
+var author;
+var subject;
 
 function getFromUrl() {
     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
     var searchUrl = document.location.search.split('&');
     console.log(searchUrl);
     // Get the query and format values
-    var author = searchUrl[0].split('=').pop();
-    var subject = searchUrl[1].split('=').pop();
+    author = searchUrl[0].split('=').pop();
+    subject = searchUrl[1].split('=').pop();
 
     console.log(author);
     console.log(subject);
   
-    displayBooks(author, subject);
+    displayBooks(author, subject, pageStartIndex);
   }
   
-function displayBooks(author, subject) {
+function displayBooks(author, subject, startIndex) {
+
     var finalUrl = 'https://openlibrary.org/search.json?';
   
+    var endUrl;
   if (author && subject) {
-    var endUrl = 'author=' + author + '&subject=' + subject;
+    endUrl = 'author=' + author + '&subject=' + subject;
   } else if (!subject) {
-    var endUrl = 'author=' + author;
+    endUrl = 'author=' + author;
   } else if (!author) {
-    var endUrl = 'subject=' + subject;
+    endUrl = 'subject=' + subject;
   } else {
     console.error('Please return to the main page and provide an author and/or a subject!');
     return;
@@ -39,9 +49,31 @@ function displayBooks(author, subject) {
   
         return response.json();
       })
-      .then(function (localPage) {
-        // write query to page so user knows what they are viewing
-        bookPage.textContent = localPage.search;
+      .then(function (searchResults) {
+
+        var firstBook = startIndex + 1;
+        var lastBook = startIndex + 10;
+        if (lastBook > searchResults.numFound) {
+            lastBook = searchResults.numFound;
+        }
+
+        document.getElementById("searchCount").textContent = "Search results found: " + searchResults.numFound + "  (Showing " + firstBook + " to " + lastBook + ")";
+
+        var html = create10Books(searchResults, startIndex);
+
+        bookPage.innerHTML = html;
+
+        if (startIndex == 0) {
+            previous.disabled = true;
+        } else {
+            previous.disabled = false;
+        }
+
+        if (startIndex + 10 > searchResults.docs.length) {
+            next.disabled = true;
+        } else {
+            next.disabled = false;
+        }
   
         console.log(localPage);
   
@@ -50,14 +82,69 @@ function displayBooks(author, subject) {
           resultContentEl.innerHTML = '<h3>No results found, search again!</h3>';
         } else {
           resultContentEl.textContent = '';
-          //for (var i = 0; i < locRes.results.length; i++) {
-            //printResults(locRes.results[i]);
-          //}
         }
       })
       .catch(function (error) {
         console.error(error);
       });
+  }
+
+  function nextPage() {
+    pageStartIndex = pageStartIndex + 10;
+    displayBooks(author, subject, pageStartIndex);
+  }
+
+  function previousPage() {
+    pageStartIndex = pageStartIndex - 10;
+    displayBooks(author, subject, pageStartIndex);
+  }
+
+  function create10Books(searchResults, start) {
+    var html = "<table class='booksTable'>";
+
+    for (let i = start; i < start + 10; i++) {
+
+        if (i < searchResults.docs.length) {
+            html += "<tr><td>";
+            html += createHtmlForBook(searchResults.docs[i]);
+            html += "</td></tr>";
+        }
+    }
+    html += "</table>";
+
+    return html;
+  }
+
+
+  function createHtmlForBook(doc) {
+    var imgurl = "https://covers.openlibrary.org/b/ID/" + doc.cover_i + "-M.jpg";
+
+    var html = "";
+    html += "<table class='bookTable'>";
+    html += "    <tr><td rowspan='0' class='imgTd'><img src='" + imgurl + "'></img></td>";
+    html += "    <td><label>Title: </label>" + doc.title + "</td>";
+    html += "    <td class='buttonTd'><input type='button' value='More Information' />&nbsp;&nbsp;";
+    html += "    <input type='button' value='Add to Favorites' /></td>";
+    html += "    </td></tr>";
+    html += "    <tr><td><label>Author: </label>" + doc.author_name + "</td></tr>";
+    html += "    <tr><td><label>Year Released: </label>" + doc.first_publish_year + "</td></tr>";
+
+    var subjectArray = doc.subject;
+    var subjects = "";
+    for (let i = 0; i < subjectArray.length; i++) {
+        subjects += subjectArray[i];
+        if (i + 1 != subjectArray.length) {
+            subjects += ", ";
+        }
+    }
+
+    html += "    <tr><td colspan='2'>";
+    html += "<table><tr><td><label>Subjects: </label></td><td>" + subjects + "</td></tr></table>";
+     
+    html += "</td></tr>";
+    html += "</table>";
+
+    return html;
   }
   
   /*console.log(authorInputVal & subjectInputVal);
